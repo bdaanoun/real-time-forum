b468cb01598d34821ad7bf343b76cf9406e0bbeb
Bilal Daanouni <daanouni00bilal@gmail.com>
2025-03-03 14:44:28 +0000

    update login page02

--- static/login.js
+++ static/login.js
@@ -3,11 +3,20 @@ import tag from "./utils/createInput.js";
 export default function login() {
 
     let body = document.body
+    let LoginDiv = tag('div', '', { 'class': 'loginPlace' })
+    let logo = tag('img', '', { 'class': 'logo', 'src': '/static/images/logo.png' })
+
+    let loginBtn = tag('button', 'Login', { 'class': 'loginBtn' })
+    let registerBtn = tag('button', 'Register', { 'class': 'registerBtn' })
+
     let lblUserName = tag('label', 'username', { 'for': 'username' })
     let username = tag('input', "", { 'placeholder': 'ur username.', 'id': 'username', 'class': 'username' })
 
-    let lblEmail = tag('label', 'Email', {'for':"Email"})
-    let Email = tag('input', '')
+    let lblPassword = tag('label', 'password', { 'for': "password" })
+    let password = tag('input', '', { 'type': 'password', 'placeholder': 'password' })
 
-    body.append(lblUserName, username)
+    let loginSubmitBtn = tag('button', 'Login', { 'class': 'loginSubmit' });
+
+    LoginDiv.append(logo, loginBtn, registerBtn, lblUserName, username, lblPassword, password, loginSubmitBtn)
+    body.append(LoginDiv)
 }
